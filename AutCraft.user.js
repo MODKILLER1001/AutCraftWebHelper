726a466a42a2ccce13591bdc52d0c3d4376751d9
Rodney Beck <denney@mantrasoftware.net>
2015-01-26 22:39:08 +1000

    feat: put username is quotes for Google searches

--- AutCraft.user.js
+++ AutCraft.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutCraft Web Helper
 // @namespace    http://mantrasoftware.net/
-// @version      0.5
+// @version      0.6
 // @description  Changes various parts of the AutCraft website to make things a little easier.
 // @author       Rodney Beck <denney@mantrasoftware.net>
 // @match        http://www.autcraft.com/dashboard/applications/detail/id/*
@@ -72,9 +72,9 @@ GM_xmlhttpRequest({
     field.append("<br><br><strong>Other Useful Links</strong>");
     field.append("<br>>> <a target=\"_blank\" href=\"http://minecraft-skin-viewer.com/player/" + username + "\">Minecraft Skin Viewer</a>");
     field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=" + username + "\">Google (username)</a>");
-    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=" + username + " youtube\">Google (YouTube)</a>");
-    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=" + username + " minecraft\">Google (Minecraft)</a>");
-    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=" + username + " ban\">Google (Ban)</a>");
-    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=" + username + " minecraft ban\">Google (Minecraft + Ban)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 youtube\">Google (YouTube)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 minecraft\">Google (Minecraft)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 ban\">Google (Ban)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 minecraft ban\">Google (Minecraft + Ban)</a>");
   }
 });
