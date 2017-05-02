6497dfea74c4181d8ce699d4b0f46ff86055abd3
Mmpf <Mmpf@users.noreply.github.com>
2017-05-02 04:27:07 +0200

    Add links when selecting new app in the sidebar

--- AutCraft.user.js
+++ AutCraft.user.js
@@ -10,32 +10,37 @@
 // @match        http://autcraft.com/dashboard/applications/application?app_id=*
 // @grant        GM_xmlhttpRequest
 // @require      http://code.jquery.com/jquery-2.1.3.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @updateURL    https://raw.githubusercontent.com/MODKILLER1001/AutCraftWebHelper/master/AutCraft.user.js
 // @downloadURL  https://raw.githubusercontent.com/MODKILLER1001/AutCraftWebHelper/master/AutCraft.user.js
 // @license      GPLv3
 // ==/UserScript==
 
-// IP address checking
-var ip = $("div.details-avatar").clone();
-ip.children().remove();
-ip = ip.text().trim();
-$("div.details-avatar").append(" >> <a target=\"_blank\" href=\"http://whatismyipaddress.com/ip/" + ip + "\">What's My IP Address</a>");
+function addAdditionalLinks(jnode) {
+    // IP address checking
+    var ip = $("div.details-avatar").clone();
+    ip.children().remove();
+    ip = ip.text().trim();
+    $("div.details-avatar").append(" >> <a target=\"_blank\" href=\"http://whatismyipaddress.com/ip/" + ip + "\">What's My IP Address</a>");
 
-// Links
-var field = $("div[hash|='bs9bnytb9d'] .input-data");
-var username = field.html();
+    // Links
+    var field = $("div[hash|='bs9bnytb9d'] .input-data");
+    var username = field.html();
 
-field.append("<br>>> <a target=\"_blank\" href=\"http://mcbans.com/player/" + username + "\">MCBans</a>");
-field.append("<br>>> <a target=\"_blank\" href=\"http://mcbouncer.com/u/" + username + "\">MCBouncer</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"http://mcbans.com/player/" + username + "\">MCBans</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"http://mcbouncer.com/u/" + username + "\">MCBouncer</a>");
 
-field.append("<br><br><strong>Other Useful Links</strong>");
-field.append("<br>>> <a target=\"_blank\" href=\"http://namemc.com/name?" + username + "\">NameMC Lookup (previous name owners and previous names)</a>");
-field.append("<br>>> <a target=\"_blank\" href=\"http://namemc.com/profile?" + username + "\">NameMC Profile (more in depth info about that player)</a>");
-field.append("<br>>> <a target=\"_blank\" href=\"http://minecraftnames.com/lookup.php?account=" + username + "\">Minecraft Name Lookup</a>");
-field.append("<br>>> <a target=\"_blank\" href=\"http://minecraft-skin-viewer.com/player/" + username + "\">Minecraft Skin Viewer</a>");
-field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=" + username + "\">Google (username)</a>");
-field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 youtube\">Google (YouTube)</a>");
-field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 minecraft\">Google (Minecraft)</a>");
-field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 ban\">Google (Ban)</a>");
-field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 minecraft ban\">Google (Minecraft + Ban)</a>");
-field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 4chan\">Google (4chan)</a>");
+    field.append("<br><br><strong>Other Useful Links</strong>");
+    field.append("<br>>> <a target=\"_blank\" href=\"http://namemc.com/name?" + username + "\">NameMC Lookup (previous name owners and previous names)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"http://namemc.com/profile?" + username + "\">NameMC Profile (more in depth info about that player)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"http://minecraftnames.com/lookup.php?account=" + username + "\">Minecraft Name Lookup</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"http://minecraft-skin-viewer.com/player/" + username + "\">Minecraft Skin Viewer</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=" + username + "\">Google (username)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 youtube\">Google (YouTube)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 minecraft\">Google (Minecraft)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 ban\">Google (Ban)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 minecraft ban\">Google (Minecraft + Ban)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.com.au/#newwindow=1&q=%22" + username + "%22 4chan\">Google (4chan)</a>");
+}
+
+waitForKeyElements("div.app_inner_user", addAdditionalLinks);
