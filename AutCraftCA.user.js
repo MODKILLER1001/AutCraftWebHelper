301ec97c6cbe5d3c7a2d0bc983af69d1b5295e84
Mmpf <Mmpf@users.noreply.github.com>
2017-05-16 05:54:29 +0200

    same for Canada

--- AutCraftCA.user.js
+++ AutCraftCA.user.js
@@ -31,10 +31,9 @@ function addAdditionalLinks(jnode) {
     field.append("<br>>> <a target=\"_blank\" href=\"http://mcbouncer.com/u/" + username + "\">MCBouncer</a>");
 
     field.append("<br><br><strong>Other Useful Links</strong>");
-    field.append("<br>>> <a target=\"_blank\" href=\"http://namemc.com/name?" + username + "\">NameMC Lookup (previous name owners and previous names)</a>");
-    field.append("<br>>> <a target=\"_blank\" href=\"http://namemc.com/profile?" + username + "\">NameMC Profile (more in depth info about that player)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"http://namemc.com/name/" + username + "\">NameMC Lookup (previous name owners and previous names)</a>");
+    field.append("<br>>> <a target=\"_blank\" href=\"http://namemc.com/profile/" + username + "\">NameMC Profile (more in depth info about that player)</a>");
     field.append("<br>>> <a target=\"_blank\" href=\"http://minecraftnames.com/lookup.php?account=" + username + "\">Minecraft Name Lookup</a>");
-    field.append("<br>>> <a target=\"_blank\" href=\"http://minecraft-skin-viewer.com/player/" + username + "\">Minecraft Skin Viewer</a>");
     field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.ca/#newwindow=1&q=" + username + "\">Google (username)</a>");
     field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.ca/#newwindow=1&q=%22" + username + "%22 youtube\">Google (YouTube)</a>");
     field.append("<br>>> <a target=\"_blank\" href=\"https://www.google.ca/#newwindow=1&q=%22" + username + "%22 minecraft\">Google (Minecraft)</a>");
